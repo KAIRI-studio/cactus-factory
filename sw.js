bcffc91eddd2c241f0785513c0deb51db8eb080d
KAIRI-studio <73matibeaver@gmail.com>
2026-09-12 22:39:58 +0900

    Align independent equipment artwork

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
-const CACHE_NAME = "cactus-factory-2026-09-12-7";
+const CACHE_NAME = "cactus-factory-2026-09-12-9";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=95",
+  "./styles.css?v=97",
   "./game.js?v=92",
   "./manifest.webmanifest",
   "./icons/apple-touch-icon.png",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
   "./assets/original-sand-factory-v2.png",
-  "./assets/equipment-air.png",
-  "./assets/equipment-sensor.png",
-  "./assets/equipment-rail.png",
+  "./assets/equipment-air.png?v=2",
+  "./assets/equipment-sensor.png?v=2",
+  "./assets/equipment-rail.png?v=2",
   "./assets/simple-bold-sprout.png",
   "./assets/cactus-normal.png",
   "./assets/cactus-rare-flower.png",
