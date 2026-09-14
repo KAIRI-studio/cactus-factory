4126af9464cf0f16f6c8763c8c3a83fcc92336cf
KAIRI-studio <73matibeaver@gmail.com>
2026-09-14 23:10:47 +0900

    Restore always-visible play-test controls

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "cactus-factory-2026-09-14-46";
+const CACHE_NAME = "cactus-factory-2026-09-14-47";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=130",
+  "./styles.css?v=132",
   "./title-v3.css?v=2",
   "./game.js?v=131",
   "./manifest.webmanifest",
