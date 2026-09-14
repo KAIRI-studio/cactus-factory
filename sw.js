5c4228ecfbd6730798c2a811be1ba75da655bb8e
KAIRI-studio <73matibeaver@gmail.com>
2026-09-15 06:45:36 +0900

    Increase legendary cactus display size

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "cactus-factory-2026-09-15-55";
+const CACHE_NAME = "cactus-factory-2026-09-15-56";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=137",
+  "./styles.css?v=138",
   "./title-v3.css?v=2",
   "./game.js?v=140",
   "./manifest.webmanifest",
