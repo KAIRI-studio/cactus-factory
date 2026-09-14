70014d8f3e115c738b15b09721901fa9596e370e
KAIRI-studio <73matibeaver@gmail.com>
2026-09-15 07:03:58 +0900

    Replace app-style navigation with illustrated factory controls

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "cactus-factory-2026-09-15-58";
+const CACHE_NAME = "cactus-factory-2026-09-15-59";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=140",
+  "./styles.css?v=141",
   "./title-v3.css?v=2",
   "./game.js?v=141",
   "./manifest.webmanifest",
