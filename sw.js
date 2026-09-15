31fab5e865bf1dc66da8d7c37066bca0032c43a8
KAIRI-studio <73matibeaver@gmail.com>
2026-09-15 15:47:54 +0900

    Keep wrong-answer shake inside the console

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "cactus-factory-2026-09-15-65";
+const CACHE_NAME = "cactus-factory-2026-09-15-66";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=146",
+  "./styles.css?v=147",
   "./title-v3.css?v=2",
   "./game.js?v=144",
   "./manifest.webmanifest",
