7753203a21f3b0bb1d49bea8ddb70439380615e8
KAIRI-studio <73matibeaver@gmail.com>
2026-09-15 12:39:06 +0900

    Keep calculation panel within mobile viewport

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "cactus-factory-2026-09-15-62";
+const CACHE_NAME = "cactus-factory-2026-09-15-63";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=143",
+  "./styles.css?v=144",
   "./title-v3.css?v=2",
   "./game.js?v=143",
   "./manifest.webmanifest",
