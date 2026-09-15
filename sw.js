3a0d8112de5e4de20f23f6122f7309618cf268dd
KAIRI-studio <73matibeaver@gmail.com>
2026-09-15 17:42:26 +0900

    Update app cache for cactus answer feedback

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "cactus-factory-2026-09-15-67";
+const CACHE_NAME = "cactus-factory-2026-09-15-68";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=148",
+  "./styles.css?v=149",
   "./title-v3.css?v=2",
   "./game.js?v=145",
   "./manifest.webmanifest",
