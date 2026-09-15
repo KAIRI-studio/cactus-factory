c51223cfa3a172d0ce52b04e71decbcbaa0262d4
KAIRI-studio <73matibeaver@gmail.com>
2026-09-16 07:54:53 +0900

    Refresh cache for tutorial sequence

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "cactus-factory-2026-09-15-76";
+const CACHE_NAME = "cactus-factory-2026-09-15-77";
 const APP_FILES = [
   "./",
   "./index.html",
   "./styles.css?v=156",
   "./title-v3.css?v=2",
-  "./game.js?v=150",
+  "./game.js?v=151",
   "./manifest.webmanifest",
   "./icons/apple-touch-icon.png",
   "./icons/icon-192.png",
