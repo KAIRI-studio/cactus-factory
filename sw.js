f080f7f20ab85b71fb247bb8f2d867cba61b5359
KAIRI-studio <73matibeaver@gmail.com>
2026-09-16 07:59:25 +0900

    Refresh cache for sound feedback

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "cactus-factory-2026-09-15-77";
+const CACHE_NAME = "cactus-factory-2026-09-15-78";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=156",
+  "./styles.css?v=157",
   "./title-v3.css?v=2",
-  "./game.js?v=151",
+  "./game.js?v=152",
   "./manifest.webmanifest",
   "./icons/apple-touch-icon.png",
   "./icons/icon-192.png",
