991bfa48dea0388a47005db25ee27e7ceeb05822
KAIRI-studio <73matibeaver@gmail.com>
2026-09-16 10:30:48 +0900

    Theme featured guide card by rarity

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "cactus-factory-2026-09-16-83";
+const CACHE_NAME = "cactus-factory-2026-09-16-84";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=162",
+  "./styles.css?v=163",
   "./title-v3.css?v=2",
   "./game.js?v=155",
   "./manifest.webmanifest",
