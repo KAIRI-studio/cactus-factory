13630c3d206cd6afddc81d9e3a3ec4b0d5f095fd
KAIRI-studio <73matibeaver@gmail.com>
2026-09-16 21:13:51 +0900

    Replace rough image fringe with smooth frame edge

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "cactus-factory-2026-09-16-99";
+const CACHE_NAME = "cactus-factory-2026-09-16-100";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=178",
+  "./styles.css?v=179",
   "./title-v3.css?v=2",
   "./game.js?v=159",
   "./manifest.webmanifest",
